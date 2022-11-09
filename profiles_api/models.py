5a56d921363bf4bcfccd15e8c587b5ad5e11a4bb
APAC\TIT5HC <fixed-term.Tien.TranVinh@vn.bosch.com>
2022-11-09 16:30:26 +0700

    fixed syntax

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -29,7 +29,7 @@ class UserProfileManager(BaseUserManager):
 
         user.is_superuser = True
         user.is_employee = True
-        user,is_staff = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
